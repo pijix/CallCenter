d6ed8ddf49868fb69f312969757a0fa9cffb7a53
Jordi Pijuan Galceran <jordi@pijuan.info>
2014-03-19 18:56:29 +0100

    MessageService definition

---
+++ CallCenter.Application/MessageService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using CallCenter.CORE.Domain;
+using CallCenter.DAL;
+
+namespace CallCenter.Application
+{
+    internal class MessageService
+    {
+
+        private DBContext _dbContext;
+
+        public MessageService(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Método que retorna todos los mensajes
+        /// </summary>
+        /// <returns>Lista de Mensajes</returns>
+        public List<Message> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Método que retorna un mensaje según Id
+        /// </summary>
+        /// <returns>Mensaje</returns>
+        public Message GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        /// <summary>
+        /// Método que retorna todos los mensajes de un usuario
+        /// </summary>
+        /// <param name="userId">Id de Usuario</param>
+        /// <returns>Lista de Mensajes</returns>
+        public List<Message> GetByUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Método que retorna todos los mensajes de un cliente
+        /// </summary>
+        /// <param name="clientId">Id de Cliente</param>
+        /// <returns>Lista de Mensajes</returns>
+        public List<Message> GetByClient(Guid clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Método para añadir un mensaje
+        /// </summary>
+        /// <param name="message">Id Usuario</param>
+        /// <returns>Deveuelve el mensage añadido</returns>
+        public Message Add(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Método para editar un mensaje
+        /// </summary>
+        /// <param name="messageId">Id de Cliente</param>
+        /// <returns>Mensaje Editado</returns>
+        public Message Update(Guid messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Método que elimina un mensaje
+        /// </summary>
+        /// <param name="messageId">Id de Cliente</param>
+        /// <returns>Si correcto o no la eliminación</returns>
+        public bool Delete(Guid messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+
+    }
+}
